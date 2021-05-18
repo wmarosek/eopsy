79bb638228c0b9eec32fb1492b3ec6dfcf8d7a12
Wojciech Marosek <wojtek.marosek@gmail.com>
2021-05-19 00:01:03 +0200

    The Dining Philosophers added

---
+++ task5/philosophers.c
@@ -0,0 +1,174 @@
+/* -------------------------------------------------------------------------------------
+ * The Dining Philosophers problem is a classic multi-process synchronization
+ * problem. The problem consists of five philosophers sitting at a table who do
+ * nothing but think and eat. Between each philosopher, there is a single fork
+ * In order to eat, a philosopher must have both forks. A problem can arise if
+ * each philosopher grabs the fork on the right, then waits for the fork on the
+ * left. In this case a deadlock has occurred, and all philosophers will starve.
+ * Also, the philosophers should be fair. Each philosopher should be able to eat
+ * as much as the rest.
+ *
+ * The Dining Philosophers
+ * ------------------------------------------------------------------------------------- */
+
+//-------------------------------------------------------------------------------------
+//   Includes:
+//-------------------------------------------------------------------------------------
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+//-------------------------------------------------------------------------------------
+//   Common variable:
+//-------------------------------------------------------------------------------------
+
+#define PHILOSOPHER_NO 5
+#define LEFT PHILOSOPHER_ID
+#define RIGHT (PHILOSOPHER_ID + 1) % PHILOSOPHER_NO
+
+pthread_t threadIds[PHILOSOPHER_NO];        // Declare thread array
+int philosophersIds[PHILOSOPHER_NO];
+static int SEMID;
+
+void eat(int i);
+void grab_forks(int left_fork_id);
+void put_away_forks(int left_fork_id);
+void *philosopher(void *num);
+void initializePhilosphers();
+void initializeThreads();
+
+
+//-------------------------------------------------------------------------------------
+//   main():
+//-------------------------------------------------------------------------------------
+int main(void) {
+    initializePhilosphers();
+    initializeThreads();
+
+    return 0;
+
+}
+
+//-------------------------------------------------------------------------------------
+//   grab_forks():
+//-------------------------------------------------------------------------------------
+void grab_forks(int left_fork_id){
+    int PHILOSOPHER_ID = left_fork_id;          // initialization for defined LEFT and RIGHT
+
+    struct sembuf sem_operation[] = {           // function for toggling the semaphore, lock the semaphore
+            { .sem_num = LEFT,  .sem_op = -1, .sem_flg = 0 },
+            { .sem_num = RIGHT, .sem_op = -1, .sem_flg = 0 },
+    };
+
+    if(semop(SEMID, sem_operation, 2) == -1){
+        perror("grab_forks(): Failed during semop error!");
+        exit(1);
+    }
+}
+
+
+//-------------------------------------------------------------------------------------
+//   put_away_forks():
+//-------------------------------------------------------------------------------------
+void put_away_forks( int left_fork_id ){
+    int PHILOSOPHER_ID = left_fork_id;          // initialization for defined LEFT and RIGHT
+
+    struct sembuf sem_operation[] = {           // function for toggling the semaphore, unlock the semaphore
+            { .sem_num = LEFT,  .sem_op = 1, .sem_flg = 0 },
+            { .sem_num = RIGHT, .sem_op = 1, .sem_flg = 0 },
+    };
+
+    if(semop(SEMID, sem_operation, 2) == -1){
+        perror("put_away_forks(): Failed during semop error!");
+        exit(1);
+    }
+    printf("Philosopher[%d] PUTS FORK [%d-%d] DOWN.\n", RIGHT, LEFT, RIGHT);
+    printf("Philosopher[%d] is THINKING.\n", RIGHT);
+
+}
+
+
+
+/* -------------------------------------------------------------------------------------
+    philosopher(void *num):
+        Pseudocode:
+        process P[i]
+        while true do {
+            THINK;
+            PICKUP(CHOPSTICK[i], CHOPSTICK[i+1 mod 5]);
+            EAT;
+            PUTDOWN(CHOPSTICK[i], CHOPSTICK[i+1 mod 5])
+        }
+------------------------------------------------------------------------------------- */
+void *philosopher(void *num)
+{
+    //Set the ID of the current philosopher
+    int PHILOSOPHER_ID = *(int *)num;
+
+    while(1)
+    {
+        printf("Philosopher[%d] is THINKING\n", PHILOSOPHER_ID);
+        sleep(1);
+        grab_forks(PHILOSOPHER_ID);		            //Philosopher grabs forks
+        eat(PHILOSOPHER_ID);
+        sleep(1);	                        //Philosopher is eating
+        put_away_forks(PHILOSOPHER_ID);	            //Philosopher puts forks down
+    }
+}
+
+//-------------------------------------------------------------------------------------
+//   initializePhilosphers():
+//-------------------------------------------------------------------------------------
+void initializePhilosphers(){
+
+    SEMID = semget(IPC_PRIVATE, PHILOSOPHER_NO, 0666);          //  create new set semaphores with 0660 specifies explicit permissions
+    if(SEMID < 0) {                           // checking if semaphores were created successfully
+        perror("initializePhilosphers(): Error while getting the semaphores.\n");
+        exit(1);
+    }
+
+    int i;
+    for(i = 0; i < PHILOSOPHER_NO; i++){                            //  initialize all semaphores to 1 - all forks are free initially
+        philosophersIds[i] = i;                                     // setting philosophersIds
+
+        if(semctl(SEMID, i, SETVAL, 1) == -1){
+            perror("initializePhilosphers(): Error while semctl!");
+            exit(1);
+        }
+    }
+}
+
+//-------------------------------------------------------------------------------------
+//   initializeThreads():
+//-------------------------------------------------------------------------------------
+void initializeThreads(){
+    int i;
+    for (i = 0; i < PHILOSOPHER_NO; i++)
+    {
+        pthread_create(&threadIds[i], NULL, philosopher, &philosophersIds[i]);  // create philosopher processes
+    }
+
+    // Join all threads
+    for (i = 0; i < PHILOSOPHER_NO; i++)
+    {
+        pthread_join(threadIds[i], NULL);                               // wait equivalent
+    }
+
+}
+
+//-------------------------------------------------------------------------------------
+//   eat():
+//-------------------------------------------------------------------------------------
+void eat(int i)
+{
+    int PHILOSOPHER_ID = i;                     // initialization for defined LEFT and RIGHT
+    sleep(2);
+    printf("Philosopher[%d] TAKES FORK %d and %d\n",
+           RIGHT, LEFT, RIGHT);
+
+    printf("Philosopher[%d] is EATING\n", RIGHT);
+}
